c81af6e625cffb38bac0c59bd035e5bab9638bb4
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-22 20:28:24 +0300

    Update Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,11 @@
+public class Faktöriyel {
+
+	public static void main(String[] args) {
+		// Sonucu hesapla
+		int sonuc = 1*2*3*4*5;
+		
+		//Ekrana yazdır
+		System.out.println("1x2x3x4x5="+sonuc);
+	}
+
+}
