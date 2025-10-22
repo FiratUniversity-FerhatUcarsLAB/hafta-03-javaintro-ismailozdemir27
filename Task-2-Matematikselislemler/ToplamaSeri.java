50fd4f8ca529c2ce7f27d74118c79a4008b31331
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-22 19:19:40 +0300

    Update ToplamaSeri.java

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,13 @@
+public class UcMesaj {
+
+	public static void main(String[] args) {
+    //	Sonucu  hesapla 
+		int sonuc = 1+2+3+4+5+6+7+8+9;
+    // Ekrana yazdır     
+	System.out.println("1+2+3+4+5+6+7+8+9=" + sonuc);
+	
+    
+  
+   
+	}
+}
