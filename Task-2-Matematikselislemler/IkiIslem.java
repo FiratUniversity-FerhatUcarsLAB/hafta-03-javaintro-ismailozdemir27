a724a2c1f2fcef30096e987164cb261a71a8074b
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-22 20:03:45 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,19 @@
+public class IkiIslem {
+
+	public static void main(String[] args) {
+		// Pay hesapla
+		double pay =(9.5*4.5)-(2.5*3);
+		
+		//Payda hesapla 
+		double payda = (45.5-3.5);
+		
+		// Bölme yap
+		double sonuc = pay/payda ;
+		
+		// Formatla ve yazdır
+		System.out.printf("%.4f\n",sonuc);         
+		
+
+	}
+
+}
