bc78590bf0bdc97df4221b8c07a974e440db66e4
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-22 18:43:34 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,13 @@
+public class UcMesaj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	
+	 System.out.println("Java’ya Hoş Geldiniz");
+     System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+     System.out.println("Programlama Eğlencelidir");
+  
+   
+	}
+}
