1407feb0d770fb5b9f69b110de7ad261d738512b
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 10:18:51 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,20 @@
+
+public class IsimKartı {
+
+	public static void main(String[] args) {
+		String ad ="Ismail";
+		String soyad ="Ozdemir";
+		String ogrNo ="250541109";
+		String bolum ="Yazılım Muh.";
+		
+		System.out.println("=================");
+		System.out.println("OGRENCI BILGILERI");
+		System.out.println("=================");
+		System.out.println("Ad    :"+ ad   );
+		System.out.println("Soyad :"+ soyad);
+		System.out.println("No    :"+ ogrNo);
+		System.out.println("Bolum :"+ bolum);
+		System.out.println("==================");
+	}
+
+}
