4ee63c9abd81d1ea5fd6af41ba4a2cbfd739c7cf
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 16:36:38 +0300

    Update HesapOzeti.java

---
+++ Task-4-StringDesenler/HesapOzeti.java
@@ -0,0 +1,23 @@
+public class HesapOzet {
+    public  static void main(String[] args) {
+        System.out.println("------------ HESAP ÖZETİ ------------");
+        System.out.println("Ürün Adı     Miktar      Birim Fiyat (TL)");
+        System.out.println("Toplam Tutar (TL)                        ");
+        System.out.println("-----------------------------------------");
+        System.out.println("-------------------");
+        System.out.println("Üzüm           3            80.00");
+        System.out.println("240.00");
+        System.out.println("Ekmek          2            10.00");
+        System.out.println("20.00");
+        System.out.println("Peynir         1            250.00");
+        System.out.println("250.00");
+        System.out.println("Çikolata       5             21.50");
+        System.out.println("107.25");
+        System.out.println("-----------------------------------------");
+        System.out.println("--------------------");
+        System.out.println("                            GENEL TOPLAM:");
+        System.out.println("617.25");
+        System.out.println("-----------------------------------------");
+        System.out.println("--------------------");
+    }
+}
