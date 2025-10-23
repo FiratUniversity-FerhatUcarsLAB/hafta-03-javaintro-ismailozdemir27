c2c174ce0108ef764f26755f4f37e1bcf5971e59
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 20:50:54 +0300

    Update AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+public class ASCIIArtIsim {
+
+	public static void main(String[] args) { 
+	    System.out.println("   ////                                                                           ////                         ");
+		System.out.println("///////////     //////////////// ////////////////////////////   //////////////  /////////    //                ");
+		System.out.println("   ///          //               //            //          //   //          //    ///        //                ");
+		System.out.println("   ///          //               //            //          //   //          //    ///        //                ");
+		System.out.println("   ///          //               //            //          //   //          //    ///        //                ");
+		System.out.println("   ///          //               //            //          //   //          //    ///        //                ");
+		System.out.println("   ///          //               //            //          //   //          //    ///        //                ");
+		System.out.println("   ///          //////////////   //            //          //   //////////////    ///        //                ");
+		System.out.println("   ///                      //   //            //          //   //          //    ///        //                ");
+    System.out.println("   ///                      //   //            //          //   //          //    ///        //                ");
+		System.out.println("   ///                      //   //            //          //   //          //    ///        //                ");
+		System.out.println("   ///                      //   //            //          //   //          //    ///        //                ");
+		System.out.println("///////////   ////////////////   //            //          //   //          //  ///////////  //////////////////");
+	
+		
+	}
+
+}
