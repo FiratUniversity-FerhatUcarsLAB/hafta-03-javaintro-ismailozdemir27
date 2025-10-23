3c4814ccb8854ccbc63f215e11810291999e2420
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 16:51:39 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,14 @@
+
+public class MilKilometreTablosu {
+
+	public static void main(String[] args) {
+		double mil =1.609;
+		System.out.println("DÖNÜŞÜM"+"                              "+ "TABLOSU");
+		System.out.println("===================================================");
+		System.out.println("MİL   "+"1"+" / "+"5"+"   /"+"10"+"   /  "+"20"+"   / "+"50");
+		System.out.println("(KM)" +mil+"/"+5*mil+"/"+10*mil+"/"+20*mil+"/"+50*mil);
+		
+
+	}
+
+}
