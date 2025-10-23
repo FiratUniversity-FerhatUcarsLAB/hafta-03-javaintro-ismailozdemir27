28f62d8fc3de108634c31b73a7f010436353b155
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 09:22:20 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,17 @@
+public class DaireHesap {
+
+	public static void main(String[] args) {
+		double r =5.5 ;
+		double pi =3.1415 ;
+		// Alan hesapla
+		double alan = pi*r*r ;
+		// Cevre hesapla 
+		double cevre = 2*pi*r ;
+		// Sonucları yazdır
+		System.out.println("Yarıcap:" + r);
+		System.out.println("Alan:" +alan);
+		System.out.println("Cevre:"+cevre);
+
+	}
+
+}
