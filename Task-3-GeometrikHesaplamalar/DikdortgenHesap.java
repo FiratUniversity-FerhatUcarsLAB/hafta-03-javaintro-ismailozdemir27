8fe350dd321d7efeb87e82a2fba343a48ea11689
ismailozdemir27 <ozdemirismail2502@gmail.com>
2025-10-23 09:37:55 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,16 @@
+public class Diktörtgen {
+
+	public static void main(String[] args) {
+		double en = 4.5;
+		double boy =7.9;
+		// Alan  hesapla
+		double alan =en*boy;
+		// Cevre hesapla
+		double cevre =2*(en+boy);
+		// Sonuçları yazdır
+		System.out.println("Alan:"+alan);
+		System.out.println("Cevre:"+ cevre);
+
+	}
+
+}
